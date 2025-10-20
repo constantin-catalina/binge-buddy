13791300be4dda17fe10fda62f50216473852bca
Constantin Catalina <catalina.constantin@student.upt.ro>
2025-10-20 10:23:57 +0300

    update backend set-up

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import 'dotenv/config';
-import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express';
-import { inngest, functions } from './inngest/index.js';
-import { serve } from 'inngest/express';
+import connectDB from "./configs/db.js";
+
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 await connectDB();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
-// API routes
-app.get('/', (req, res) => res.send('Server is running!'))
-app.use('/api/inngest', serve({ client: inngest, functions }));
+// API Routes
+app.get('/', (req, res) => {  
+    res.send('Server is running!');  
+}); 
 
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
+app.use('/api/inngest', serve({ client: inngest, functions }))
+
+app.listen(PORT, () => {  
+    console.log(`Server is running on http://localhost:${PORT}`);  
+});  
